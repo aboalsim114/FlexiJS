3b2435c9e88a7d8fb3c43712b3fd28306ea333b1
aboalsim114 <aboalsim114@gmail.com>
2024-09-13 00:29:18 +0200

    Refactorisation du code pour simplifier les vérifications de type et améliorer la lisibilité.

--- src/Objects/ObjetScript.js
+++ src/Objects/ObjetScript.js
@@ -1,73 +1,46 @@
 export function ObjetScript() {
     return {
         cloneObjet: (sourceObj = {}, destObj = {}) => {
-            if (typeof sourceObj !== 'object' || sourceObj === null || typeof destObj !== 'object' || destObj === null) {
+            if (!isValidObject(sourceObj) || !isValidObject(destObj)) {
                 throw new Error("Les paramètres doivent être des objets valides.");
             }
-
-            for (let key in sourceObj) {
-                if (sourceObj.hasOwnProperty(key)) { 
-                    destObj[key] = sourceObj[key];
-                }
-            }
+            Object.keys(sourceObj).forEach(key => {
+                destObj[key] = sourceObj[key];
+            });
             return destObj;
         }, 
 
         addItemToObjet: (targetObj, item) => {
-            if (typeof targetObj !== 'object' || targetObj === null) {
+            if (!isValidObject(targetObj)) {
                 throw new Error("L'objet cible doit être un objet valide.");
             }
-
-            for (let key in item) {
-                if (item.hasOwnProperty(key)) {
-                    targetObj[key] = item[key];
-                }
-            }
+            Object.assign(targetObj, item);
             return targetObj;
         },
 
         deleteItemFromObjet: (targetObj, itemKeys) => {
-            if (typeof targetObj !== 'object' || targetObj === null) {
+            if (!isValidObject(targetObj)) {
                 throw new Error("L'objet cible doit être un objet valide.");
             }
-
-            for (let key of itemKeys) {
+            itemKeys.forEach(key => {
                 if (targetObj.hasOwnProperty(key)) {
                     delete targetObj[key];  
                 }
-            }
-
+            });
             return targetObj;
         },
 
 
         fusionnerObjets: (obj1, obj2) => {
-            if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+            if (!isValidObject(obj1) || !isValidObject(obj2)) {
                 throw new Error("Les deux paramètres doivent être des objets valides.");
             }
-
-            const resultat = { ...obj1 };
-
-            for (let key in obj2) {
-                if (obj2.hasOwnProperty(key)) {
-                    resultat[key] = obj2[key];
-                }
-            }
-
-            return resultat;
+            return { ...obj1, ...obj2 };
         },
-
-
-
     };
 }
 
-
-
-
-
-
-
-
-
+function isValidObject(obj) {
+    return typeof obj === 'object' && obj !== null;
+}
 
